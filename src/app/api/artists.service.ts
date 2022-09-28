e0e20ebd99f05b940f78c4cbfb3494ac5fa0c1ac
Infinical <inficalvin@gmail.com>
2022-09-28 12:18:50 +0300

    Add response types and increse bundle budget

--- src/app/api/artists.service.ts
+++ src/app/api/artists.service.ts
@@ -1,23 +1,29 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-
+interface SearchResponse{
+  data: any[];
+  next: string;
+  total: number;
+}
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class ArtistsService {
 
   constructor(private http: HttpClient) { }
 
   searchArtist(name:string){
-    return this.http.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search`,{params: new HttpParams().set('q',`artist:"${name}"`).set('response_type','token')})
+    return this.http.get<SearchResponse>(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search`,{params: new HttpParams().set('q',`artist:"${name}"`).set('response_type','token')})
   }
 
   getTopSongs(id:string){
-    return this.http.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/top`)
+    return this.http.get<SearchResponse>(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/top`)
   }
 
   getAlbums(id:string){
-    return this.http.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/albums`)
+    return this.http.get<SearchResponse>(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/albums`)
   }
 }
