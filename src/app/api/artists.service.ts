0917c1801d3e0d2208275e185913cb8fbc7171e5
Infinical <inficalvin@gmail.com>
2022-09-28 12:24:55 +0300

    Updates

--- src/app/api/artists.service.ts
+++ src/app/api/artists.service.ts
@@ -1,29 +1,72 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
-interface SearchResponse{
+interface SearchResponse {
   data: any[];
   next: string;
   total: number;
 }
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
 export class ArtistsService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  searchArtist(name:string){
-    return this.http.get<SearchResponse>(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search`,{params: new HttpParams().set('q',`artist:"${name}"`).set('response_type','token')})
+  searchArtist(name: string): Observable<any> {
+    return this.http
+      .get<SearchResponse>(
+        `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search`,
+        {
+          params: new HttpParams()
+            .set('q', `artist:"${name}"`)
+            .set('response_type', 'token'),
+        }
+      )
+      .pipe(
+        retry(2),
+        catchError(this.handleError));
   }
 
-  getTopSongs(id:string){
-    return this.http.get<SearchResponse>(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/top`)
+  getTopSongs(id: string): Observable<any> {
+    return this.http
+      .get<SearchResponse>(
+        `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/top`
+      )
+      .pipe(
+        retry(2),
+        catchError(this.handleError));
   }
 
-  getAlbums(id:string){
-    return this.http.get<SearchResponse>(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/albums`)
+  getAlbums(id: string) :Observable<any> {
+    return this.http
+      .get<SearchResponse>(
+        `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${id}/albums`
+      )
+      .pipe(
+        retry(2),
+        catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
+    );
   }
 }
